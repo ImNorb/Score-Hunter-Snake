e33b9fc065e77a6f42637c94fb5539f7dd54f4a8
Norbert Szucs <mail.norbert.szucs@gmail.com>
2019-09-29 02:43:57 +0200

    C#:Backend Scoring

---
+++ Score Hunter Snake/Score Hunter Snake/Scoring.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel;
+
+namespace Score_Hunter_Snake {
+    class Scoring : INotifyPropertyChanged {
+        private int score;
+        public int Score {
+            get { return score; }
+            set {
+                score = value;
+                OnPropertyChanged("Score");
+            }
+        }
+
+        private int endScore;
+        public int EndScore {
+            get { return endScore; }
+            set {
+                endScore = value;
+                OnPropertyChanged("EndScore");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyName) {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
